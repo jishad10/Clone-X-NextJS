201c9b7ad6bf284909ade1cbcf51b5414cdae381
jahidul islam <jahidilislamjishad2019@gmail.com>
2024-10-18 20:45:47 +0600

    Upload post

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -16,6 +16,8 @@ export default function Input() {
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
   const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [text, setText] = useState('');
+  const [postLoading, setPostLoading] = useState(false);
   const imagePickRef = useRef(null);
 
   if (!isSignedIn || !isLoaded) {
@@ -64,6 +66,29 @@ export default function Input() {
     );
   };
 
+  const handleSubmit = async () => {
+    setPostLoading(true);
+    const response = await fetch('/api/post/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userMongoId: user.publicMetadata.userMongoId,
+        name: user.fullName,
+        username: user.username,
+        text,
+        profileImg: user.imageUrl,
+        image: imageFileUrl,
+      }),
+    });
+    setPostLoading(false);
+    setText('');
+    setSelectedFile(null);
+    setImageFileUrl(null);
+    // location.reload();
+  };
+
   return (
     <div className='flex border-b border-gray-200 p-3 space-x-3 w-full'>
       <img
@@ -74,8 +99,10 @@ export default function Input() {
       <div className='w-full divide-y divide-gray-200'>
         <textarea
           className='w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700 '
-          placeholder='What’s happening?'
+          placeholder='What’s happening'
           rows='2'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         ></textarea>
         {selectedFile && (
           <img
@@ -93,7 +120,7 @@ export default function Input() {
         <div className='flex items-center justify-between pt-2.5'>
           <HiOutlinePhotograph
             className='h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer'
-            onClick={() => imagePickRef.current && imagePickRef.current.click()}
+            onClick={() => imagePickRef.current.click()}
           />
           <input
             type='file'
@@ -103,8 +130,9 @@ export default function Input() {
             onChange={addImageToPost}
           />
           <button
-            disabled
+            disabled={text.trim() === '' || postLoading || imageFileUploading}
             className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+            onClick={handleSubmit}
           >
             Post
           </button>
