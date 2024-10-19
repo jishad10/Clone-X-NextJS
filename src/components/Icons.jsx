a17eb47e5edd5462e7a25ccc42e1566c5e36a5dc
jahidul islam <jahidilislamjishad2019@gmail.com>
2024-10-19 21:32:46 +0600

    complete post, user and search

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,39 +1,44 @@
 'use client';
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash, HiHeart } from 'react-icons/hi';
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from 'react-icons/hi';
 import { useState, useEffect } from 'react';
 import { useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
-
+import { modalAtom, postIdAtom } from '@/atom/modalAtom';
+import { useRecoilState } from 'recoil';
 export default function Icons({ post }) {
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState(post.likes || []);
+  const [open, setOpen] = useRecoilState(modalAtom);
+  const [postId, setPostId] = useRecoilState(postIdAtom);
   const { user } = useUser();
   const router = useRouter();
 
-  const likePost = async () => {
+  const likePost = () => {
     if (!user) {
       return router.push('/sign-in');
     }
-
-    const likeResponse = await fetch('/api/post/like', {
+    const like = fetch('/api/post/like', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ postId: post._id }),
     });
-
-    if (likeResponse.ok) {
-      if (isLiked) {
-        setLikes(likes.filter((like) => like !== user.publicMetadata.userMongoId));
-      } else {
-        setLikes([...likes, user.publicMetadata.userMongoId]);
-      }
-      setIsLiked(!isLiked);
+    if (like && isLiked) {
+      setLikes(
+        likes.filter((like) => like !== user.publicMetadata.userMongoId)
+      );
+    }
+    if (like && !isLiked) {
+      setLikes([...likes, user.publicMetadata.userMongoId]);
     }
   };
-
   useEffect(() => {
     if (user && likes?.includes(user.publicMetadata.userMongoId)) {
       setIsLiked(true);
@@ -52,7 +57,7 @@ export default function Icons({ post }) {
           },
           body: JSON.stringify({ postId: post._id }),
         });
-        if (res.ok) {
+        if (res.status === 200) {
           location.reload();
         } else {
           alert('Error deleting post');
@@ -63,17 +68,32 @@ export default function Icons({ post }) {
 
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
-      <HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100' />
+      <div className='flex items-center'>
+        <HiOutlineChat
+          className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
+          onClick={() => {
+            if (!user) {
+              router.push('/sign-in');
+            } else {
+              setOpen(!open);
+              setPostId(post._id);
+            }
+          }}
+        />
+        {post.comments.length > 0 && (
+          <span className='text-xs'>{post.comments.length}</span>
+        )}
+      </div>
       <div className='flex items-center'>
         {isLiked ? (
           <HiHeart
             onClick={likePost}
-            className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100'
+            className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100'
           />
         ) : (
           <HiOutlineHeart
             onClick={likePost}
-            className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+            className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
           />
         )}
         {likes.length > 0 && (
@@ -85,7 +105,7 @@ export default function Icons({ post }) {
       {user && user.publicMetadata.userMongoId === post.user && (
         <HiOutlineTrash
           onClick={deletePost}
-          className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
+          className='h-8 w-8 cursor-pointer rounded-full  transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100'
         />
       )}
     </div>
